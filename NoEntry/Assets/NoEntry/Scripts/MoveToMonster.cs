8913067b23022cb2d1c54f2409a039d452b4f042
keea <heea44@naver.com>
2019-05-19 09:32:22 +0900

    그래픽스 중간과제
    그래픽스 중간과제

---
+++ NoEntry/Assets/NoEntry/Scripts/MoveToMonster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMonster : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
